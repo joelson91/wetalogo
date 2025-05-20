e0091ae1ec4bf6d785696a8ee917b8853df53efe
joelson <joelsonandradep@gmail.com>
2025-05-20 10:50:03 -0300

    feat: add index, product_details and checkout views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from core.models import Product
 
-# Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, "index.html", {'products': products})
+
+
+def product_details(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, "product.html", {'product': product})
+
+
+def checkout(request):
+    return render(request, "checkout.html")
